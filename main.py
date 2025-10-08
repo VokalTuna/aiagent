24b16ceaada21bbae6938c95f2a739b5eb01540b
VokalTuna <knutolavs@pm.me>
2025-10-08 14:56:29 +0200

    Completed AI agent.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 
+from config import MAX_ITERS
 from prompts import system_prompt
 from call_functions import available_functions, call_function
 
@@ -34,22 +35,25 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    # Keep track of messages
-    # Repeat the generate content function at minimum 20 times
-    # If it returns responses, fetch it's candidates content and add
-    # them to messages
-    # Quit if it returns text.
-    for _ in range(0,20):
-        generated_answer = generate_content(client, messages, verbose)
-        if isinstance(generated_answer, str):
-            print(generated_answer)
-            break
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
 
 
 
 
 def generate_content(client,messages, verbose):
-    new_messages = messages
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
@@ -61,7 +65,7 @@ def generate_content(client,messages, verbose):
 
     # Iterate over the candidates, and add them to messages.
     for candidate in response.candidates:
-        new_messages.append(candidate.content)
+        messages.append(candidate.content)
 
     if not response.function_calls:
         return response.text
@@ -75,9 +79,11 @@ def generate_content(client,messages, verbose):
             raise Exception("empty function call result")
         if verbose:
             print(f"-> {function_call_response}")
-        function_responses.append(function_call_result)
+        function_responses.append(function_call_result.parts[0])
     if not function_responses:
         raise Exception("no function reponses generated, exiting.")
+
+    messages.append(types.Content(role="user",parts=function_responses))
 
 if __name__ == "__main__":
     main()
